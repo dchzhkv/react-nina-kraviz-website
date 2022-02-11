03b0deeb3c8ca26c0e03dd648aea159c49fca648
Daria_ch <drchzhkv@gmail.com>
2022-02-11 19:00:56 +0200

    update package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,18 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+              // Override the file regex for Sass
+              sassRuleTest: /\.global\.s(a|c)ss$/,
+              // Override the file regex for CSS modules
+              sassRuleModulesTest: /\.mod\.s(a|c)ss$/,
+            },
+          },
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sass`
     ]
 }
