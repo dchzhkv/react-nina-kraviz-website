8289c73e6a8daf141b2fd5a0e2da4983796b3d39
Daria_ch <drchzhkv@gmail.com>
2022-02-14 19:00:00 +0200

    fix: gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,25 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {
-              // Override the file regex for Sass
-              sassRuleTest: /\.global\.s(a|c)ss$/,
-              // Override the file regex for CSS modules
-              sassRuleModulesTest: /\.mod\.s(a|c)ss$/,
-            },
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        // Override the file regex for Sass
+        sassRuleTest: /\.global\.s(a|c)ss$/,
+        // Override the file regex for CSS modules
+        sassRuleModulesTest: /\.mod\.s(a|c)ss$/,
+        cssLoaderOptions: {
+          modules: {
+            exportLocalsConvention: 'camelCaseOnly',
           },
-        `gatsby-plugin-image`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sass`
-    ]
-}
+        },
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
+  ],
+};
