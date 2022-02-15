daad92cc2ead930e8f57b0aa3cddf0fa5ac8e926
Daria_ch <drchzhkv@gmail.com>
2022-02-15 16:34:39 +0200

    feat: show tour page

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from '../Header';
-import Main from '../Main';
+import { Header, Main, Tour } from '../';
 
 const Home = (): JSX.Element => {
   return (
     <>
       <Header />
       <Main />
+      <Tour />
     </>
   );
 };
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 export { default as Header } from './Header';
 export { default as Main } from './Main';
+export { default as Tour } from './Tour';
