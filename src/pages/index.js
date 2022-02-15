0754116c97c1afcabbdc2099f0b7a96fcd178226
Daria_ch <drchzhkv@gmail.com>
2022-02-15 16:35:32 +0200

    feat: get title from graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,26 @@
 import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import { Home } from '../containers';
 import '../styles/reset.css';
 import '../styles/index.scss';
 
-const Index = () => {
+function Index() {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <main>
-      <title>Nina Kraviz</title>
+      <title>{data.site.siteMetadata.title}</title>
       <Home />
     </main>
   );
-};
+}
 
 export default Index;
