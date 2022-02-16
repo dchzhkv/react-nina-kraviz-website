7025fb7700044f75fcd6529df2745c819485a129
Daria_ch <drchzhkv@gmail.com>
2022-02-16 18:07:40 +0200

    feat: one page scroll

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
 import { Header, Main, Tour, Contacts, About } from '../';
 
 const Home = (): JSX.Element => {
   return (
     <>
       <Header />
-      <Main />
-      <Tour />
-      <Contacts />
-      <About />
+
+      <ReactFullpage
+        fixedElements={'header'}
+        normalScrollElements={'.normalScroll'}
+        render={() => {
+          return (
+            <div id="fullpage-wrapper">
+              <div className="section section1">
+                <Main />
+              </div>
+              <div className="section">
+                <Tour />
+              </div>
+              <div className="section">
+                <Contacts />
+              </div>
+              <div className="section">
+                <About />
+              </div>
+            </div>
+          );
+        }}
+      />
     </>
   );
 };
