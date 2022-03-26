11889595959a352ce308a91d22bcb6649ca329d9
Daria Chyzhykova <drchzhkv@gmail.com>
2022-03-26 21:49:16 +0200

    fix: Tour component

--- src/containers/Tour/index.tsx
+++ src/containers/Tour/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as styles from './index.module.scss';
 import { useStaticQuery, graphql } from 'gatsby';
 import { TourItem } from './components/';
 
@@ -25,15 +24,14 @@ const Tour = (): JSX.Element => {
     <section>
       <h2 className="title container">Techno 2022 tour</h2>
 
-      <ul className={`${'content normalScroll'} ${styles.list}`}>
+      <ul>
         {tourData.allTourJson.edges.map((item) => (
           <TourItem key={item.node.id} items={item.node} />
         ))}
       </ul>
-
-      <div className={styles.scrollDownIcon}>
+      {/* <div className={styles.scrollDownIcon}>
         <span>☟</span>
-      </div>
+      </div> */}
     </section>
   );
 };
