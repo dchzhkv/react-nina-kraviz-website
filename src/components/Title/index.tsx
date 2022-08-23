4c8686e401cfee5b8464d6d70f2408a6d8ab4211
Daria Chyzhykova <drchzhkv@gmail.com>
2022-08-23 13:47:59 +0300

    feat: title animation

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as styles from './index.module.scss';
 
 interface Props {
   title: string;
+  isAnimHover: boolean;
   handleMouseEnter?: () => void;
   handleMouseLeave?: () => void;
   styles?: string;
 }
 
 const Title = (props: Props): JSX.Element => {
+  const handleHover = () => {
+    return `${styles.title} ${styles.titleClone} ${props.isAnimHover ? styles.titleAnim : null} ${props.styles}`;
+  };
   return (
-    <div className={styles.container} onMouseEnter={props.handleMouseEnter} onMouseLeave={props.handleMouseLeave}>
+    <div
+      className={`${props.styles} ${styles.container}`}
+      onMouseEnter={props.handleMouseEnter}
+      onMouseLeave={props.handleMouseLeave}
+    >
       <div className={styles.title}>{props.title}</div>
-      <div className={`${styles.title} ${styles.titleClone} ${props.styles}`}>{props.title}</div>
+      <div className={handleHover()}>{props.title}</div>
     </div>
   );
 };
