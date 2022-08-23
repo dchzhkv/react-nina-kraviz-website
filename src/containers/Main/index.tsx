4dc3a704f60003455e340316f2c2ce0a5a04a9f4
Daria Chyzhykova <drchzhkv@gmail.com>
2022-08-23 13:51:07 +0300

    fix: header

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Header from '../Header';
 import * as styles from './index.module.scss';
 
 const Main = (): JSX.Element => {
   return (
-    <section className={styles.main}>
-      <div className="container">
-        <h1 className={styles.main__title}>Nina Kraviz</h1>
+    <section data-scroll-section>
+      <div className={styles.main}>
+        <Header />
+        <div className="container">
+          <h1 className={styles.main__title}>Nina Kraviz</h1>
+        </div>
       </div>
     </section>
   );
