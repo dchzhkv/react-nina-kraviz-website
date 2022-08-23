c8fd0c2a2d20f02b587c86f1c127aecf59033072
Daria Chyzhykova <drchzhkv@gmail.com>
2022-08-23 13:45:01 +0300

    fix: tour item

--- src/containers/Tour/index.tsx
+++ src/containers/Tour/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { TourItem } from './components/';
+import gsap from 'gsap';
+import SplitText from '../../utils/Split.min';
+import useOnScreen from '../../hooks/useOnScreen';
 
 const Tour = (): JSX.Element => {
   const tourData = useStaticQuery(graphql`
@@ -20,9 +23,43 @@ const Tour = (): JSX.Element => {
     }
   `);
 
+  const ref = useRef(null);
+
+  const [reveal, setReveal] = useState(false);
+  const onScreen = useOnScreen(ref, 0.5);
+
+  useEffect(() => {
+    if (onScreen) setReveal(onScreen);
+  }, [onScreen]);
+
+  useEffect(() => {
+    if (reveal) {
+      const split = new SplitText('#title', {
+        type: 'lines',
+        linesClass: 'lineChildren',
+      });
+      const splitParent = new SplitText('#title', {
+        type: 'lines',
+        linesClass: 'lineParent',
+      });
+      gsap.fromTo(
+        split.lines,
+        { y: 200 },
+        {
+          duration: 1,
+          y: 0,
+          stagger: 0.1,
+          ease: 'power2',
+        }
+      );
+    }
+  }, [reveal]);
+
   return (
-    <section>
-      <h2 className="title container">Techno 2022 tour</h2>
+    <section data-scroll-section>
+      <h2 className={`${reveal ? 'is-reveal' : null} title container anim-title`} ref={ref} id="title">
+        Techno 2022 tour
+      </h2>
 
       <ul>
         {tourData.allTourJson.edges.map((item) => (
