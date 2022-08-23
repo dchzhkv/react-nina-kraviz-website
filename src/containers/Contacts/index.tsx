316c564d538dad0e2ed295bca25dcfd264b71c01
Daria Chyzhykova <drchzhkv@gmail.com>
2022-08-23 13:49:28 +0300

    feat: contacts animation

--- src/containers/Contacts/index.tsx
+++ src/containers/Contacts/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as styles from './index.module.scss';
+import gsap from 'gsap';
+import SplitText from '../../utils/Split.min';
 import { useStaticQuery, graphql } from 'gatsby';
 import ContactsItem from './components/ContactsItem';
+import useOnScreen from '../../hooks/useOnScreen';
 
 const Contacts = (): JSX.Element => {
   const data = useStaticQuery(graphql`
@@ -19,9 +22,43 @@ const Contacts = (): JSX.Element => {
     }
   `);
 
+  const ref = useRef(null);
+
+  const [reveal, setReveal] = useState(false);
+  const onScreen = useOnScreen(ref, 0.5);
+
+  useEffect(() => {
+    if (onScreen) setReveal(onScreen);
+  }, [onScreen]);
+
+  useEffect(() => {
+    if (reveal) {
+      const split = new SplitText('#title', {
+        type: 'lines',
+        linesClass: 'lineChildren',
+      });
+      const splitParent = new SplitText('#title', {
+        type: 'lines',
+        linesClass: 'lineParent',
+      });
+      gsap.fromTo(
+        split.lines,
+        { y: 200 },
+        {
+          duration: 1,
+          y: 0,
+          stagger: 0.1,
+          ease: 'power2',
+        }
+      );
+    }
+  }, [reveal]);
+
   return (
-    <section className="container">
-      <h2 className="title">Contacts</h2>
+    <section className="container" data-scroll-section>
+      <h2 className={`${reveal ? 'is-reveal' : null} title anim-title`} ref={ref} id="title">
+        Contacts
+      </h2>
       <div>
         {data.allContactsJson.edges.map((item) => (
           <ContactsItem key={item.node.id} items={item.node} />
